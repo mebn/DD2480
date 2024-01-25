88ad4d73d315586c86bac25b138d8bcec73b2ad0
Marcus Nilszén <nilszen.marcus@gmail.com>
2024-01-25 11:11:14 +0100

    Fix: code structure (#21)

--- assignment1_DECIDE/src/tests/TestLICXX.java
+++ assignment1_DECIDE/src/tests/TestLICXX.java
@@ -1,11 +1,16 @@
+package src.tests;
+
 import static org.junit.Assert.*;
 
 import org.junit.Assert;
 import org.junit.Test;
 
+import src.LaunchInterceptor;
+
 public class TestLICXX {
     @Test
     public void testLICxx() {
+        LaunchInterceptor li = new LaunchInterceptor(0, null, null, null, null);
         Assert.assertTrue(1==1);
     }
 }
