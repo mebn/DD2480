c7f905adf257a4b9ec285b0b4de2747b61168459
maals <maals@kth.se>
2024-01-25 10:37:51 +0100

    Changed LIC to 0 index

--- assignment1_DECIDE/LaunchInterceptor.java
+++ assignment1_DECIDE/LaunchInterceptor.java
@@ -6,6 +6,28 @@ enum operator {
   NOTUSED
 }
 
+class LaunchParameters {
+  public double LENGTH1; // Length in LICs 0, 7, 12
+  public double RADIUS1; // Radius in LICs 1, 8, 13
+  public double EPSILON; // Deviation from PI in LICs 2, 9
+  public double AREA1; // Area in LICs 3, 10, 14
+  public int Q_PTS; // Number of consecutive points in LIC 4
+  public int QUADS; // Number of quadrants in LIC 4
+  public double DIST; // Distance in LIC 6
+  public int N_PTS; // Number of consecutive pts. in LIC 6
+  public int K_PTS; // Number of intermediate pts. in LICs 7, 12
+  public int A_PTS; // Number of intermediate pts. in LICs 8, 13
+  public int B_PTS; // Number of intermediate pts. in LICs 8, 13
+  public int C_PTS; // Number of intermediate pts. in LIC 9
+  public int D_PTS; // Number of intermediate pts. in LIC 9
+  public int E_PTS; // Number of intermediate pts. in LICs 10, 14
+  public int F_PTS; // Number of intermediate pts. in LICs 10, 14
+  public int G_PTS; // Number of intermediate pts. in LIC 11
+  public double LENGTH2; // Maximum length in LIC 12
+  public double RADIUS2; // Maximum radius in LIC 13
+  public double AREA2; // Maximum area in LIC 14
+}
+
 public class LaunchInterceptor {
   private final int NUMPOINTS;
   private final Point[] POINTS;
@@ -22,6 +44,10 @@ public class LaunchInterceptor {
   }
 
   public boolean decide() {
+    return true;
+  }
+
+  private boolean checkLIC_0() {
     return true;
   }
 
@@ -81,34 +107,8 @@ public class LaunchInterceptor {
     return true;
   }
 
-  private boolean checkLIC_15() {
-    return true;
-  }
-
   public static void main(String[] args) {
 
   }
 
-}
-
-class LaunchParameters {
-  public double LENGTH1; // Length in LICs 0, 7, 12
-  public double RADIUS1; // Radius in LICs 1, 8, 13
-  public double EPSILON; // Deviation from PI in LICs 2, 9
-  public double AREA1; // Area in LICs 3, 10, 14
-  public int Q_PTS; // Number of consecutive points in LIC 4
-  public int QUADS; // Number of quadrants in LIC 4
-  public double DIST; // Distance in LIC 6
-  public int N_PTS; // Number of consecutive pts. in LIC 6
-  public int K_PTS; // Number of intermediate pts. in LICs 7, 12
-  public int A_PTS; // Number of intermediate pts. in LICs 8, 13
-  public int B_PTS; // Number of intermediate pts. in LICs 8, 13
-  public int C_PTS; // Number of intermediate pts. in LIC 9
-  public int D_PTS; // Number of intermediate pts. in LIC 9
-  public int E_PTS; // Number of intermediate pts. in LICs 10, 14
-  public int F_PTS; // Number of intermediate pts. in LICs 10, 14
-  public int G_PTS; // Number of intermediate pts. in LIC 11
-  public double LENGTH2; // Maximum length in LIC 12
-  public double RADIUS2; // Maximum radius in LIC 13
-  public double AREA2; // Maximum area in LIC 14
 }
