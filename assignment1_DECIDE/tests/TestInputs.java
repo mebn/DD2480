4b7501e6897755b18015995616d202ca34005228
Noak Jönsson <noakj@kth.se>
2024-01-25 10:21:53 +0100

    doc: create skeleton for LIC and Input tests

---
+++ assignment1_DECIDE/tests/TestInputs.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class TestInputs {
+
+    @Test
+    public void TestPositiveInput (LaunchInterceptor li) {
+        // implement a test for correct input
+    }
+    public void TestNegativeInput (LaunchInterceptor li) {
+        // implement a test for faulty input
+    }
+}
---
+++ assignment1_DECIDE/tests/TestLICXX.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestLICXX {
+
+    @Test
+    public void testLICxx(LaunchInterceptor li) {
+        // define your test
+        // use assert
+    }
+}
