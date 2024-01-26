e99c7abdc3806eadcac3d11a6d7d1cefd47af98c
marcusalstrom <maals@kth.se>
2024-01-26 22:07:38 +0100

    Add: LIC2 (#47)
    * First logic #23
    
    * Add LIC2 functionality first version (#23)

--- assignment1_DECIDE/src/LaunchInterceptor.java
+++ assignment1_DECIDE/src/LaunchInterceptor.java
@@ -17,6 +17,7 @@ public class LaunchInterceptor {
   private final LaunchParameters PARAMETERS;
   private final operator[][] LCM;
   private final boolean[] PUV;
+  private final double PI = 3.1415926535;
 
   public LaunchInterceptor(int numpoints, Point2D[] points, LaunchParameters params, operator[][] lcm, boolean[] puv) {
     this.NUMPOINTS = numpoints;
@@ -38,32 +39,60 @@ public class LaunchInterceptor {
     return false;
   }
 
-  private boolean checkLIC_1() {
+  public boolean checkLIC_1() {
     return true;
   }
 
-  
+  /**
+   * Checks if there exists at least one set of three consecutive data points
+   * which form an angle such that:
+   * angle < (PI − EPSILON) or angle > (PI + EPSILON)
+   * The second of the three consecutive points is always the vertex of the angle.
+   * If either the first point or the last point (or both) coincides with the
+   * vertex, the angle is undefined
+   * and the LIC is not satisfied by those three points.
+   * (0 ≤ EPSILON < PI)
+   *
+   * @return true if the condition is satisfied, false otherwise
+   */
   public boolean checkLIC_2() {
-    return true;
+    for (int i = 0; i < NUMPOINTS - 2; i++) {
+      Point2D first = POINTS[i];
+      Point2D second = POINTS[i + 1];
+      Point2D third = POINTS[i + 2];
+      // If first or third point coincide with second point, angle is undefined
+      if (first.equals(second) || third.equals(second))
+        continue;
+      double firstAngle = Math.atan2(first.getY() - second.getY(), first.getX() - second.getX());
+      double thirdAngle = Math.atan2(third.getY() - second.getY(), third.getX() - second.getX());
+      // firstAngle = (firstAngle > 0) ? firstAngle : 2 * pi + firstAngle;
+      // thirdAngle = (thirdAngle > 0) ? thirdAngle : 2 * pi + thirdAngle;
+      double angle = Math.abs(firstAngle - thirdAngle);
+
+      if (angle < PI - PARAMETERS.EPSILON || angle > PI + PARAMETERS.EPSILON) {
+        return true;
+      }
+    }
+    return false;
   }
 
   public boolean checkLIC_3() {
     double enclosedArea;
 
-    for (int i = 0; i < POINTS.length-2; i++) {
+    for (int i = 0; i < POINTS.length - 2; i++) {
       Point2D p1 = POINTS[i];
       double x1 = p1.getX();
       double y1 = p1.getY();
 
-      Point2D p2 = POINTS[i+1];
+      Point2D p2 = POINTS[i + 1];
       double x2 = p2.getX();
       double y2 = p2.getY();
 
-      Point2D p3 = POINTS[i+2];
+      Point2D p3 = POINTS[i + 2];
       double x3 = p3.getX();
       double y3 = p3.getY();
 
-      enclosedArea = Math.abs(x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2))/2;
+      enclosedArea = Math.abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2;
       if (enclosedArea > PARAMETERS.AREA1) {
         return true;
       }
@@ -115,7 +144,7 @@ public class LaunchInterceptor {
         return true;
       }
     }
-    
+
     return false;
   }
 
@@ -125,8 +154,9 @@ public class LaunchInterceptor {
 
   public boolean checkLIC_7() {
     for (int i = 1 + PARAMETERS.K_PTS; i < POINTS.length; i++) {
-      if (POINTS.length < 3) return false;
-      
+      if (POINTS.length < 3)
+        return false;
+
       if (POINTS[i].distance(POINTS[i - PARAMETERS.K_PTS - 1]) > PARAMETERS.LENGTH1)
         return true;
     }
