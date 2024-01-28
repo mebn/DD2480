e376f746fe13113701ee1a29f37c0bf642a5b177
dj-sourbrough <56027726+dj-sourbrough@users.noreply.github.com>
2024-01-28 16:08:41 +0100

    Fixed Tests for LIC2 Issue/46 (#49)
    * fix LIC2 tests
    
    * fixed comments
    
    ---------
    
    Co-authored-by: Noak Jönsson <noakj@kth.se>

--- assignment1_DECIDE/src/tests/TestLIC_2.java
+++ assignment1_DECIDE/src/tests/TestLIC_2.java
@@ -1,7 +1,9 @@
 package src.tests;
 
 import java.awt.Point;
+
 import static org.junit.Assert.*;
+import java.lang.Math;
 
 import org.junit.Test;
 
@@ -10,60 +12,62 @@ import src.LaunchParameters;
 
 public class TestLIC_2 {
 
-    double pi = 3.1415926535;
-    // init epsilon. all vertex angles v must be such that v < pi/2, v > 3pi/2
+    /*
+     * There exists at least one set of three consecutive data points which form an angle such that: 
+     *      - angle < (PI − EPSILON) or angle > (PI + EPSILON) 
+     *      - The second of the three consecutive points is always the vertex of the angle.
+     *      - If either the first point or the last point (or both) coincides with the vertex,
+     *              the angle is undefined and the LIC is not satisfied by those three points.  (0 ≤ EPSILON < PI)
+     * 
+     * Test Requirements:
+     *      1. angle(x,x,y) = undefined
+     *      2. angle(x,y,z) < pi - epsilon
+     *      3. pi - epsilon <= angle(x,y,z) =< pi + epsilon
+     *      4. angle(x,y,z) > pi + epsilon
+     *      5. angle(x,y,z) != angle(y,x,z) 
+     */
+
+    LaunchParameters params = new LaunchParameters();
+
+    final double epsilon = Math.PI / 2;
+    final Point[] pointsUndefinedAngle = {new Point(0,0), new Point(0,0),  new Point(1,0)};
+    final Point[] points45deg = {new Point(1,0), new Point(0, 0), new Point(1,1)};
+    final Point[] points90deg = {new Point(0,0), new Point(1,0), new Point(1,1)};
+    final Point[] points315deg = {new Point(1,0), new Point(0,0), new Point(1,-1)};
 
     @Test
-    public void testLIC_2_AngleIsLessThanBoundary() {
-        /*
-         * Asserts True
-         * Vertex angle = pi/4
-         */
-
-        LaunchParameters params = new LaunchParameters();
-        params.EPSILON = pi/4;
-        
-        Point[] datapoints = {new Point(0,0), new Point(1,0), new Point(0,1)};
-
-        LaunchInterceptor li = new LaunchInterceptor(3, datapoints, params, null, null);
+    public void testUndefinedAngle() {
+        /* TR1 */
+        params.EPSILON = epsilon;
+        LaunchInterceptor li = new LaunchInterceptor(3, pointsUndefinedAngle, params, null, null);
         boolean result = li.checkLIC_2();
+        assertFalse(result);
+    }
 
+    @Test
+    public void testAngleLessThan90deg() {
+        /* TR2 */
+        params.EPSILON = epsilon;
+        LaunchInterceptor li = new LaunchInterceptor(3, points45deg, params, null, null);
+        boolean result = li.checkLIC_2();
         assertTrue(result);
     }
 
     @Test
-    public void testLIC_2_AngleEqualsBoundary() {
-        /*
-         * Assert False
-         * Vertex angle = 90
-         */
-
-        LaunchParameters params = new LaunchParameters();
-        params.EPSILON = pi/4;
-
-        Point[] datapoints = {new Point(1,0), new Point(0,0), new Point(0,1)};
-
-        LaunchInterceptor li = new LaunchInterceptor(3, datapoints, params, null, null);
+    public void testAngle90deg() {
+        /* TR3,TR5 */
+        params.EPSILON = epsilon;
+        LaunchInterceptor li = new LaunchInterceptor(3, points90deg, params, null, null);
         boolean result = li.checkLIC_2();
-
         assertFalse(result);
     }
 
     @Test
-    public void testLIC_2_AngleGreaterThanBoundary() {
-        /*
-         * Assert false
-         * Vertex angle = 3/4*pi
-         */
-
-        LaunchParameters params = new LaunchParameters();
-        params.EPSILON = pi/4;
-
-        Point[] datapoints = {new Point(1,0), new Point(0,0), new Point(1,-1)};
-
-        LaunchInterceptor li = new LaunchInterceptor(3, datapoints, params, null, null);
+    public void testAngleMoreThan270deg() {
+        /* TR4 */
+        params.EPSILON = epsilon;
+        LaunchInterceptor li = new LaunchInterceptor(3, points315deg, params, null, null);
         boolean result = li.checkLIC_2();
-
-        assertFalse(result);
+        assertTrue(result);
     }
 }
