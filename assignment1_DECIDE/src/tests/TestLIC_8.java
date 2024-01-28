718c06c661c51f0e0834fcd0726f50eb52e3f138
maals <maals@kth.se>
2024-01-28 19:55:07 +0100

    Updated separating points to more extreme values to catch more errors

--- assignment1_DECIDE/src/tests/TestLIC_8.java
+++ assignment1_DECIDE/src/tests/TestLIC_8.java
@@ -35,9 +35,9 @@ public class TestLIC_8 {
         params.B_PTS = 1;
         Point2D.Double[] datapoints = {
                 new Point2D.Double(1.0, 4.0),
-                new Point2D.Double(1.0, 1.0), // A_PTS
+                new Point2D.Double(99.0, 99.0), // A_PTS
                 new Point2D.Double(3.0, 2),
-                new Point2D.Double(1.0, 1.0), // B_PTS
+                new Point2D.Double(99.0, 99.0), // B_PTS
                 new Point2D.Double(1.0, 0.0)
         };
 
@@ -61,9 +61,9 @@ public class TestLIC_8 {
         params.B_PTS = 1;
         Point2D.Double[] datapoints = {
                 new Point2D.Double(1.0, 4.1),
-                new Point2D.Double(1.0, 1.0), // A_PTS
+                new Point2D.Double(99.0, 99.0), // A_PTS
                 new Point2D.Double(3.1, 2),
-                new Point2D.Double(1.0, 1.0), // B_PTS
+                new Point2D.Double(99.0, 99.0), // B_PTS
                 new Point2D.Double(1.0, -0.1)
         };
 
@@ -87,9 +87,9 @@ public class TestLIC_8 {
         params.B_PTS = 1;
         Point2D.Double[] datapoints = {
                 new Point2D.Double(1.0, 3.9),
-                new Point2D.Double(1.0, 1.0), // A_PTS
+                new Point2D.Double(99.0, 99.0), // A_PTS
                 new Point2D.Double(2.9, 2),
-                new Point2D.Double(1.0, 1.0), // B_PTS
+                new Point2D.Double(99.0, 99.0), // B_PTS
                 new Point2D.Double(1.0, 0.1)
         };
 
@@ -112,7 +112,7 @@ public class TestLIC_8 {
         Point2D.Double[] datapoints = {
                 new Point2D.Double(0.0, 1.9),
                 new Point2D.Double(1.9, 0),
-                new Point2D.Double(1.0, 1.0), // B_PTS
+                new Point2D.Double(99.0, 99.0), // B_PTS
                 new Point2D.Double(0.0, -1.9)
         };
 
@@ -135,9 +135,9 @@ public class TestLIC_8 {
         params.B_PTS = 2;
         Point2D.Double[] datapoints = {
                 new Point2D.Double(0.0, 1.9),
-                new Point2D.Double(1.0, 1.0), // A_PTS
+                new Point2D.Double(99.0, 99.0), // A_PTS
                 new Point2D.Double(1.9, 0),
-                new Point2D.Double(1.0, 1.0), // B_PTS
+                new Point2D.Double(99.0, 99.0), // B_PTS
                 new Point2D.Double(0.0, -1.9)
         };
 
