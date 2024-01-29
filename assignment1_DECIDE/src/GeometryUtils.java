b400db437ae52839cab3495483c5d050567d351c
maals <maals@kth.se>
2024-01-29 18:03:13 +0100

    threePointAngle now returns counterclockwise angle

--- assignment1_DECIDE/src/GeometryUtils.java
+++ assignment1_DECIDE/src/GeometryUtils.java
@@ -13,14 +13,21 @@ public class GeometryUtils {
   /**
    * Calculates the absolute value of the angle between three points
    * using the middle point as the vertex of the angle.
+   * It shouldn't matter that we only return the counter clock-wise angle (read LIC9) right??
+   * This can probably be made better with the new Point class. I just didn't look at it fully yet
    * @param a the first point
    * @param b the second point which is the vertex of the angle
    * @param c the third point
-   * @return the absolute value of the angle between the three points
+   * @return the absolute value of the counter clock-wise angle between the three points
    */
   public static double threePointAngle(Point2D a, Point2D b, Point2D c) {
+    // Compute the angle from positive x-axis to point a and b [-pi, pi]
     double firstAngle = Math.atan2(a.getY() - b.getY(), a.getX() - b.getX());
     double thirdAngle = Math.atan2(c.getY() - b.getY(), c.getX() - b.getX());
+
+    // Format both angles to go counter clock-wise [0, 2pi]
+    firstAngle = (firstAngle < 0) ? 2 * Math.PI + firstAngle : firstAngle;
+    thirdAngle = (thirdAngle < 0) ? 2 * Math.PI + thirdAngle : thirdAngle;
     return Math.abs(firstAngle - thirdAngle);
   }
 }
