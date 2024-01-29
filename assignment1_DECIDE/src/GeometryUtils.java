4ea8a402e16cc22f572dff280395ff31054ed000
maals <maals@kth.se>
2024-01-29 18:45:00 +0100

    Updated to use new Point class

--- assignment1_DECIDE/src/GeometryUtils.java
+++ assignment1_DECIDE/src/GeometryUtils.java
@@ -11,23 +11,14 @@ public class GeometryUtils {
   }
   
   /**
-   * Calculates the absolute value of the angle between three points
-   * using the middle point as the vertex of the angle.
-   * It shouldn't matter that we only return the counter clock-wise angle (read LIC9) right??
-   * This can probably be made better with the new Point class. I just didn't look at it fully yet
+   * Calculates the absolute value of the angle between a and c
+   * using the middle point b as the vertex of the angle.
    * @param a the first point
    * @param b the second point which is the vertex of the angle
    * @param c the third point
    * @return the absolute value of the counter clock-wise angle between the three points
    */
   public static double threePointAngle(Point a, Point b, Point c) {
-    // Compute the angle from positive x-axis to point a and b [-pi, pi]
-    double firstAngle = Math.atan2(a.getY() - b.getY(), a.getX() - b.getX());
-    double thirdAngle = Math.atan2(c.getY() - b.getY(), c.getX() - b.getX());
-
-    // Format both angles to go counter clock-wise [0, 2pi]
-    firstAngle = (firstAngle < 0) ? 2 * Math.PI + firstAngle : firstAngle;
-    thirdAngle = (thirdAngle < 0) ? 2 * Math.PI + thirdAngle : thirdAngle;
-    return Math.abs(firstAngle - thirdAngle);
+    return Math.abs(a.subtract(b).angle(c.subtract(b)));
   }
 }
