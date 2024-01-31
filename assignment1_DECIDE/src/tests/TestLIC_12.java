4ab2ba26bc7600c0aec8f65c94ffcf52669cf09e
dj-sourbrough <56027726+dj-sourbrough@users.noreply.github.com>
2024-01-31 13:15:46 +0100

    Add: issue #17 Adds unittests for LIC12 (#55)
    * fix LIC2 tests
    
    * fixed comments
    
    * add: unit tests for LIC10
    
    * add: unittests for LIC12
    
    * fix: removed changes to LIC2 and LIC12
    
    * Fix: Implement new Point class
    
    ---------
    
    Co-authored-by: Noak Jönsson <noakj@kth.se>

---
+++ assignment1_DECIDE/src/tests/TestLIC_12.java
@@ -0,0 +1,75 @@
+package src.tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+import src.LaunchInterceptor;
+import src.LaunchParameters;
+import src.Point;
+
+public class TestLIC_12 {
+    /* 
+     * There exists two point u,v separated by K_PTS such that dist(u,v) > LENGTH1
+     *      AND
+     * There exists two points w,t (can be u,v) separated by K_PTS such that dist(w,t) < LENGTH2
+     * NUMPOINTS > 3
+     * LENGTH2 >= 0
+     */
+
+    LaunchParameters params = new LaunchParameters();
+    Point[] points = {new Point(0,0), new Point(1,0), new Point(1,1), new Point(0,1)};
+
+    @Test
+    public void testFalseLessThanLENGTH1() {
+        params.LENGTH1 = 2;
+        params.LENGTH2 = 2;
+        params.K_PTS = 1;
+        /* dist(u,v) = sqrt(2) */
+
+        LaunchInterceptor li = new LaunchInterceptor(4, points, params, null, null);
+        boolean result = li.checkLIC_12();
+        assertFalse(result);
+    }
+
+    @Test
+    public void testFalseGreaterThanLENGTH2() {
+        params.LENGTH1 = 1;
+        params.LENGTH2 = 1;
+        params.K_PTS = 1;
+        /* dist(u,v) = sqrt(2) */
+
+        LaunchInterceptor li = new LaunchInterceptor(4, points, params, null, null);
+        boolean result = li.checkLIC_12();
+        assertFalse(result);
+    }
+
+    @Test
+    public void testTrueLENGTH1LENGTH2() {
+        params.LENGTH1 = 1;
+        params.LENGTH2 = 2;
+        params.K_PTS = 1;
+        /* dist(u,v) = sqrt(2) */
+
+        LaunchInterceptor li = new LaunchInterceptor(4, points, params, null, null);
+        boolean result = li.checkLIC_12();
+        assertTrue(result);
+
+    }
+
+    @Test
+    public void testFalseNUMPOINTS() {
+        params.LENGTH1 = 1;
+        params.LENGTH2 = 2;
+        params.K_PTS = 1;
+        
+        Point[] points = {new Point(0,0), new Point(1,0), new Point(1,1)};
+        /* dist(u,v) = sqrt(2) */
+
+        LaunchInterceptor li = new LaunchInterceptor(3, points, params, null, null);
+        boolean result = li.checkLIC_12();
+        assertFalse(result);
+    }
+}
