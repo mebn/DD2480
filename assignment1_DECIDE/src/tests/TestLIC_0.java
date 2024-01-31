6d1b4a2c7d1c5190e7df3d4cbaba749ce5dac4f8
Oliver Lindgren <oliver.lindgren@outlook.com>
2024-01-31 14:17:29 +0100

    Doc: Added comments describing what TestLIC_0 is testing

--- assignment1_DECIDE/src/tests/TestLIC_0.java
+++ assignment1_DECIDE/src/tests/TestLIC_0.java
@@ -10,11 +10,17 @@ import src.LaunchInterceptor;
 import src.LaunchParameters;
 
 public class TestLIC_0 {
-    @Test
-
     /**
-     * Test cases for LIC 0
+     * Test cases for LIC 0. LIC 0 should be true iff:
+     * There exists at least one set of two consecutive data points that are a distance greater than
+     * the length, LENGTH1, apart.
      */
+
+
+    /*
+     * Tests that LIC0 is true if there are two consecutive data points that are further apart than 2 units.
+     */
+    @Test
     public void testLIC0True() {
         LaunchParameters params = new LaunchParameters();
         params.LENGTH1 = 2;
@@ -28,6 +34,9 @@ public class TestLIC_0 {
         Assert.assertTrue(res);
     }
 
+    /*
+     * Tests that LIC0 returns false if there are no consecutive data points further apart than 4 units.
+     */
     @Test
     public void testLIC0False() {
         LaunchParameters params = new LaunchParameters();
