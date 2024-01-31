adbb1b2276af52a2740fc8f0b046f3ea4433214d
Oliver Lindgren <oliver.lindgren@outlook.com>
2024-01-31 14:28:10 +0100

    Doc: Added comments describing what TestLIC_4 and TestLIC_5 is testing

--- assignment1_DECIDE/src/tests/TestLIC_4.java
+++ assignment1_DECIDE/src/tests/TestLIC_4.java
@@ -9,6 +9,19 @@ import src.LaunchInterceptor;
 import src.LaunchParameters;
 
 public class TestLIC_4 {
+    
+    /**
+     * LIC 4 should return true iff:
+     * There exists at least one set of Q PTS consecutive data points that lie in more than QUADS
+     * quadrants. Where there is ambiguity as to which quadrant contains a given point, priority
+     * of decision will be by quadrant number, i.e., I, II, III, IV. For example, the data point (0,0)
+     * is in quadrant I, the point (-l,0) is in quadrant II, the point (0,-l) is in quadrant III, the point
+     * (0,1) is in quadrant I and the point (1,0) is in quadrant I.
+     */
+
+     /**
+      * Tests that LIC 4 returns true if such a set of Q PTS exist.
+      */
     @Test
     public void testLIC4True() {
         LaunchParameters params = new LaunchParameters();
@@ -22,7 +35,9 @@ public class TestLIC_4 {
 
         Assert.assertTrue(res);
     }
-
+    /**
+     * Tests that LIC 4 returns false if such a set doesn't exist.
+     */
     @Test
     public void testLIC4False() {
         LaunchParameters params = new LaunchParameters();
--- assignment1_DECIDE/src/tests/TestLIC_5.java
+++ assignment1_DECIDE/src/tests/TestLIC_5.java
@@ -10,6 +10,15 @@ import src.LaunchInterceptor;
 import src.LaunchParameters;
 
 public class TestLIC_5 {
+    /**
+     * LIC 5 should return true iff:
+     * There exists at least one set of two consecutive data points, (X[i],Y[i]) and (X[j],Y[j]), such
+     * that X[j] - X[i] < 0. (where i = j-1)
+     */
+
+     /**
+      * Tests that LIC 5 returns true for two points where x1 = 1 and x2 = 0.
+      */
     @Test
     public void testLIC5True() {
         LaunchParameters params = new LaunchParameters();
@@ -23,6 +32,9 @@ public class TestLIC_5 {
         Assert.assertTrue(res);
     }
 
+    /**
+     * Tests that LIC 5 returns false if there exists no two consecutive such points.
+     */
     @Test
     public void testLIC5False() {
         LaunchParameters params = new LaunchParameters();
