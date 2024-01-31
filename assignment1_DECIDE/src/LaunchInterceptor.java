97f9dc6ba7c12bacb2cdb14957a0bbef867f4d24
Noak Jönsson <noakj@kth.se>
2024-01-31 13:27:06 +0100

    Fix: Implement GeometryUtils

--- assignment1_DECIDE/src/LaunchInterceptor.java
+++ assignment1_DECIDE/src/LaunchInterceptor.java
@@ -371,18 +371,10 @@ public class LaunchInterceptor {
   
     for (int i = 0; i < NUMPOINTS - 2 - E_PTS - F_PTS; i++) {
       Point p1 = POINTS[i];
-      double x1 = p1.getX();
-      double y1 = p1.getY();
-  
       Point p2 = POINTS[i + E_PTS + 1];
-      double x2 = p2.getX();
-      double y2 = p2.getY();
-  
       Point p3 = POINTS[i + E_PTS + F_PTS + 2];
-      double x3 = p3.getX();
-      double y3 = p3.getY();
-  
-      double enclosedArea = Math.abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2;
+      
+      double enclosedArea = GeometryUtils.triangleArea(p1, p2, p3);
   
       if (enclosedArea > PARAMETERS.AREA1) req1 = true;
       if (enclosedArea < PARAMETERS.AREA2) req2 = true;
