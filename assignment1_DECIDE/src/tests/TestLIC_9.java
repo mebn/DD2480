a8cd92d2cbf787452e70aa5ec9a2be1be3fb2213
Oliver Lindgren <oliver.lindgren@outlook.com>
2024-02-01 19:00:13 +0100

    Add: Commenst describing the tests for LIC_9

--- assignment1_DECIDE/src/tests/TestLIC_9.java
+++ assignment1_DECIDE/src/tests/TestLIC_9.java
@@ -10,17 +10,20 @@ import src.LaunchInterceptor;
 import src.LaunchParameters;
 
 public class TestLIC_9 {
-    /*
-    There exists at least one set of three data points separated by exactly C PTS and D PTS 
-    consecutive intervening points, respectively, that form an angle such that:
-    angle < (PI − EPSILON) or angle > (PI + EPSILON)
-    The second point of the set of three points is always the vertex of the angle. 
-    If either the first point or the last point (or both) coincide with the vertex, 
-    the angle is undefined and the LIC is not satisfied by those three points. 
-    When NUMPOINTS < 5, the condition is not met.
-    1≤C PTS,1≤D PTS     C PTS+D PTS ≤ NUMPOINTS−3
+    /**
+    * LIC 9 should return true iff:
+    * There exists at least one set of three data points separated by exactly C PTS and D PTS 
+    * consecutive intervening points, respectively, that form an angle such that:
+    * angle < (PI − EPSILON) or angle > (PI + EPSILON)
+    * The second point of the set of three points is always the vertex of the angle. 
+    * If either the first point or the last point (or both) coincide with the vertex, 
+    * the angle is undefined and the LIC is not satisfied by those three points. 
+    * When NUMPOINTS < 5, the condition is not met.
     */
     
+    /**
+     * Checks that true is returned for a simple case. 
+     */
     @Test
     public void testLIC9True() {
         
@@ -41,6 +44,9 @@ public class TestLIC_9 {
         Assert.assertTrue(res);
     }
 
+    /**
+     * Checks that false is returned when angle is exactly equal. to Pi-Epsilon
+     */
     @Test
     public void testLIC9False() {
         LaunchParameters params = new LaunchParameters();
@@ -60,11 +66,14 @@ public class TestLIC_9 {
         Assert.assertFalse(res);
     }
 
+    /**
+     * Checks that false is returned when epsilon is near 0 and angle is PI
+     */
     @Test
     public void testLIC9FalseAnglePiDegrees() {
         LaunchParameters params = new LaunchParameters();
         
-        // angle = 90 degrees
+        
         Point[] datapoints = {new Point(0,2), new Point(99,99), new Point(0,0), new Point(99,99), new Point(0,-2)};
 
         params.EPSILON = 0.000001;    // Floating point problems here if you set epsilon = 0. It should still return false but returns true
@@ -77,11 +86,14 @@ public class TestLIC_9 {
         Assert.assertFalse(res);
     }
 
+    /**
+     * Tests that true is returned when the angle is just under PI and epsilon is near 0.
+     */
     @Test
     public void testLIC9TrueAngleJustUnderPiDegrees() {
         LaunchParameters params = new LaunchParameters();
         
-        // angle = 90 degrees
+        
         Point[] datapoints = {new Point(-0.1,2.0), new Point(99,99), new Point(0,0), new Point(99,99), new Point(0,-2)};
 
         params.EPSILON = 0.000001;
@@ -94,6 +106,9 @@ public class TestLIC_9 {
         Assert.assertTrue(res);
     }
 
+    /**
+     * Tests that false is returned when there is less than 5 points.
+     */
     @Test
     public void testLIC9FalseWhenLessThen5Points() {
         LaunchParameters params = new LaunchParameters();
