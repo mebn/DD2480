04f447e9076a19c2a10e807611d3dac10a1457fe
Marcus Nilszén <nilszen.marcus@gmail.com>
2024-02-06 10:33:37 +0100

    Assignment2 code skeleton (#116)
    * Add: code skeleton for assignment 2
    
    * Fix: name change
    
    * Fix: ran cargo fmt
    
    * Add: test example
    
    * Add: more skeleton code
    
    * Fix: ran cargo fmt

--- assignment2/src/ci.rs
+++ assignment2/src/ci.rs
@@ -1,9 +1,25 @@
 pub struct CI {
-    path_repo: String,
-    path_log: String,
+    path_repo: String, // DD2480/assignment2/temp/repo-1322
+    path_log: String,  // DD2480/assignment2/logs/repo-1322
+    status: Status,
+}
+
+pub struct Status {
+    build_status: bool,
+    test_status: bool,
 }
 
 impl CI {
+    pub fn new(path_repo: String, path_log: String) -> Self {
+        Self {
+            path_repo,
+            path_log,
+            status: Status {
+                build_status: false,
+                test_status: false,
+            },
+        }
+    }
     pub fn build(&self) {}
     pub fn test(&self) {}
 }
--- assignment2/src/main.rs
+++ assignment2/src/main.rs
@@ -4,6 +4,23 @@ mod ci;
 mod github;
 mod repository;
 
+const REPO_PATH: &str = "/tmp/repo";
+const LOG_PATH: &str = "/tmp/repo";
+
 fn main() {
     println!("Hello, world!");
+    // server
+}
+
+fn handle_webhook() {
+    // new github struct
+    // parse webhook
+    // new repo struct (url: github.webhook_data.url)
+    // repo.clone
+    // repo.checkout
+    // new CI struct(REPO_PATH + repo.commitID, LOG_PATH)
+    // CI.build()
+    // CI.test()
+    // repo.cleanup()
+    // github.send_commit_status(CI.status)
 }
