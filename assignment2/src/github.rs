c79995672b2fd507e1012ac5eb1d859cfe5b48e4
Marcus Nilszén <nilszen.marcus@gmail.com>
2024-02-06 10:29:28 +0100

    Add: more skeleton code

--- assignment2/src/github.rs
+++ assignment2/src/github.rs
@@ -1,10 +1,14 @@
-struct WebhookData {}
+use crate::ci::Status;
+
+struct WebhookData {
+    repo: String,
+}
 
 pub struct Github {
-    webhook_data: WebhookData,
+    webhook_data: Option<WebhookData>,
 }
 
 impl Github {
-    pub fn parse_webhook(&self) {}
-    pub fn send_commit_status(&self) {}
+    pub fn parse_webhook(&mut self, data: String) {}
+    pub fn send_commit_status(&self, status: &Status) {}
 }
--- assignment2/src/repository.rs
+++ assignment2/src/repository.rs
@@ -1,15 +1,19 @@
 pub struct Repository {
-    url: String,
-    path: String,
+    url: String,    
+    path: String,   // Path to local repo
     branch: String,
+    commit_id: String,
 }
 
 impl Repository {
+    pub fn new() -> Self {
+        todo!("Implement this function")
+    
+    }
     pub fn clone(&self) {}
     pub fn checkout(&self) {}
     pub fn cleanup(&self) {}
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -18,6 +22,5 @@ mod tests {
     #[test]
     fn test_clone() {
         assert!(true);
-    
     }
 }
