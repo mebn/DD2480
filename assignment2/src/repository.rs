b571d05483736368953cf839d4ba2e0cc61d5c1c
maals <maals@kth.se>
2024-02-06 13:09:56 +0100

    Add: Documentation

--- assignment2/src/repository.rs
+++ assignment2/src/repository.rs
@@ -7,7 +7,20 @@ pub struct Repo {
     commit_id: String,
 }
 
+/// Represents a repository.
 impl Repo {
+    /// Creates a new instance of `Repo`.
+    ///
+    /// # Arguments
+    ///
+    /// * `url` - The URL of the repository.
+    /// * `path` - The local path where the repository will be cloned.
+    /// * `branch` - The branch to be checked out.
+    /// * `commit_id` - The commit ID to be checked out.
+    ///
+    /// # Returns
+    ///
+    /// A new instance of `Repo`.
     pub fn new(url: String, path: String, branch: String, commit_id: String) -> Self {
         Self {
             url,
@@ -16,6 +29,9 @@ impl Repo {
             commit_id,
         }
     }
+
+    /// Clones the repository from the URL specified in `self.url`
+    /// to the path specified in `self.path`.
     pub fn clone(&self) {
         println!("Cloning repo from {} to {}", self.url, self.path);
         let repo = match Repository::clone(&self.url, &self.path) {
@@ -23,8 +39,16 @@ impl Repo {
             Err(e) => panic!("failed to clone: {}", e),
         };
     }
-    pub fn checkout(&self) {}
-    pub fn cleanup(&self) {}
+
+    /// Checks out a specific branch or commit.
+    pub fn checkout(&self) {
+        // TODO: Implement checkout logic
+    }
+
+    /// Cleans up the repository.
+    pub fn cleanup(&self) {
+        // TODO: Implement cleanup logic
+    }
 }
 
 #[cfg(test)]
