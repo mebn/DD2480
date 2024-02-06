ec67bc0c26558bb6003bf8588511957c80b838c7
maals <maals@kth.se>
2024-02-06 12:18:22 +0100

    Add: clone functionality

--- assignment2/src/repository.rs
+++ assignment2/src/repository.rs
@@ -1,15 +1,28 @@
-pub struct Repository {
+use git2::Repository;
+
+pub struct Repo {
     url: String,
     path: String, // Path to local repo
     branch: String,
     commit_id: String,
 }
 
-impl Repository {
-    pub fn new() -> Self {
-        todo!("Implement this function")
+impl Repo {
+    pub fn new(url: String, path: String, branch: String, commit_id: String) -> Self {
+        Self {
+            url,
+            path,
+            branch,
+            commit_id,
+        }
     }
-    pub fn clone(&self) {}
+    pub fn clone(&self) {
+        println!("Cloning repo from {} to {}", self.url, self.path);
+        let repo = match Repository::clone(&self.url, &self.path) {
+            Ok(repo) => repo,
+            Err(e) => panic!("failed to clone: {}", e),
+        };
+    }
     pub fn checkout(&self) {}
     pub fn cleanup(&self) {}
 }
