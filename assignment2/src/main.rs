004ae045ec1f70635c9e5c253c9125ed04cf8b16
Marcus Nilszén <nilszen.marcus@gmail.com>
2024-02-06 09:26:12 +0100

    Add: test example

--- assignment2/src/main.rs
+++ assignment2/src/main.rs
@@ -1,3 +1,5 @@
+#![warn(missing_docs)]
+
 mod ci;
 mod github;
 mod repository;
--- assignment2/src/repository.rs
+++ assignment2/src/repository.rs
@@ -7,4 +7,17 @@ pub struct Repository {
 impl Repository {
     pub fn clone(&self) {}
     pub fn checkout(&self) {}
+    pub fn cleanup(&self) {}
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_clone() {
+        assert!(true);
+    
+    }
 }
