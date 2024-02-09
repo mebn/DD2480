f89545b94195d93af460dfb8ca87454312e131d0
Marcus Nilszén <nilszen.marcus@gmail.com>
2024-02-09 11:32:28 +0100

    Add: paths to LOGS and CI directories

--- assignment2/src/main.rs
+++ assignment2/src/main.rs
@@ -10,12 +10,25 @@ pub mod github;
 pub mod repository;
 pub mod routes;
 
+use lazy_static::lazy_static;
 use routes::github_webhook::github_webhook;
 
-// const REPO_PATH: &str = "/tmp/repo";
-// const LOG_PATH: &str = "/tmp/repo";
+// Global variables to paths.
+// Cannot use const because of std::env::current_dir().
+lazy_static! {
+    /// The path to the LOGS directory.
+    /// Used to store the logs from build and test commands.
+    pub static ref LOGS_PATH: std::path::PathBuf = std::env::current_dir()
+        .unwrap().join("LOGS");
+
+    /// The path to the CI directory.
+    /// Used to store the repositories that the server clones.
+    pub static ref CI_PATH: std::path::PathBuf = std::env::current_dir()
+        .unwrap().join("CI");
+}
 
 #[tokio::main]
+/// Starts the CI server and handles all the routes.
 async fn main() {
     let app = Router::new().route("/github_webhook", post(github_webhook));
 
