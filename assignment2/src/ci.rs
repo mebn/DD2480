ba3d3c8827337835f331a9b79d9228c392a4a5b5
maals <maals@kth.se>
2024-02-09 17:37:31 +0100

    Add: documentation

--- assignment2/src/ci.rs
+++ assignment2/src/ci.rs
@@ -6,12 +6,19 @@ pub struct CI {
     status: Status,
 }
 
+/// Represents the status of the build and test processes.
 pub struct Status {
     build_status: bool,
     test_status: bool,
 }
 
 impl CI {
+    /// Constructs a new `CI` instance.
+    ///
+    /// # Arguments
+    ///
+    /// * `path_repo` - A string that holds the path to the local repository.
+    /// * `path_log` - A string that holds the path to where the test should be logged.
     pub fn new(path_repo: String, path_log: String) -> Self {
         Self {
             path_repo,
@@ -24,6 +31,9 @@ impl CI {
     }
     pub fn build(&self) {}
 
+    /// Runs `cargo test --verbose` on the repo specified in `self.path_repo`,
+    /// updates the test status in `self.status`,
+    /// and logs the test output to the file specified by `self.path_log`.
     pub fn test(&mut self) -> Result<(), std::io::Error> {
         let output = Command::new("cargo")
             .arg("test")
@@ -43,6 +53,7 @@ impl CI {
         Ok(())
     }
 
+    /// Logs the output of the tests to a file.
     fn log_to_file(&self, bytes: &[u8]) -> Result<(), std::io::Error> {
         File::create(&self.path_log)?.write_all(&bytes)?;
         Ok(())
@@ -52,7 +63,7 @@ impl CI {
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    /// Tests that the `CI` struct is constructed correctly.
     #[test]
     fn test_ci_fields() {
         let ci = CI::new("test/directory".to_string(), "test/directory".to_string());
@@ -60,6 +71,8 @@ mod tests {
         assert_eq!(ci.path_log, "test/directory");
     }
 
+    /// Tests that a passing test suite updates the `test_status` field in `Status` to `true`
+    /// and logs the test output to a file.
     #[test]
     fn test_ci_tests_pass() {
         let mut ci = CI::new(
@@ -73,6 +86,8 @@ mod tests {
         std::fs::remove_file("tests/logs/log_test_pass.txt").unwrap();
     }
 
+    /// Tests that a failing test suite updates the `test_status` field in `Status` to `false`
+    /// and logs the test output to a file.
     #[test]
     fn test_ci_tests_fail() {
         let mut ci = CI::new(
