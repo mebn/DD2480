b1a96cfb384a1da7bbaab481e57895810cae7c95
Marcus Nilszén <nilszen.marcus@gmail.com>
2024-02-09 23:21:48 +0100

    Add: frontend, path consts, update .gitignore

--- assignment2/src/main.rs
+++ assignment2/src/main.rs
@@ -3,21 +3,43 @@
 
 #![warn(missing_docs)]
 
-use axum::{routing::post, Router};
-
 pub mod ci;
 pub mod github;
 pub mod repository;
 pub mod routes;
 
+use axum::{
+    http::StatusCode,
+    routing::{get, post},
+    Router,
+};
+use lazy_static::lazy_static;
+use routes::frontend::{list_all_commits, list_log_files_for_commit, show_file};
 use routes::github_webhook::github_webhook;
 
-// const REPO_PATH: &str = "/tmp/repo";
-// const LOG_PATH: &str = "/tmp/repo";
+// Global variables to paths.
+// Cannot use const because of std::env::current_dir().
+// Not ideal, but it works for now.
+lazy_static! {
+    /// The path to the directory where the logs are stored.
+    pub static ref LOGS_PATH: std::path::PathBuf = std::env::current_dir()
+        .unwrap().join("CI_LOGS");
 
+    /// The path to the directory where the repositories are cloned.
+    pub static ref REPO_PATH: std::path::PathBuf = std::env::current_dir()
+        .unwrap().join("CI_REPOS");
+}
+
+/// Starts the CI server and handles all the routes.
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/github_webhook", post(github_webhook));
+    let app = Router::new()
+        .route("/github_webhook", post(github_webhook))
+        .route("/", get(list_all_commits))
+        .route("/:commit", get(list_log_files_for_commit))
+        .route("/:commit/:file", get(show_file))
+        // we just ignore favicon for now
+        .route("/favicon.ico", get(|| async { StatusCode::NOT_FOUND }));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8007").await.unwrap();
     println!("Listening on {}", listener.local_addr().unwrap());
--- assignment2/src/routes/mod.rs
+++ assignment2/src/routes/mod.rs
@@ -1,3 +1,4 @@
 //! This module contains all the routes for the application.
 
+pub mod frontend;
 pub mod github_webhook;
